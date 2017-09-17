c5545a1522e730768b749339307757584289bb6d
Tim <timpwbaker@gmail.com>
2017-09-17 20:28:08 +0100

    Implement Action Cable
    This commit implements action cable to update the views with each vote
    cast in a poll.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -6,4 +6,10 @@ class Poll < ApplicationRecord
   has_many :options, inverse_of: :poll, dependent: :destroy
 
   accepts_nested_attributes_for :options
+
+  def notify_client_side
+    options.each do |option|
+      ActionCable.server.broadcast "poll_notifications_channel", option_id: option.id, votes_cast: option.vote_count
+    end
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://pop-poll.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://pop-poll.herokuapp.com', 'http://pop-poll.herokuapp.com']
 end
